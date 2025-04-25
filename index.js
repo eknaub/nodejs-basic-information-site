83284426fb13e85e369fc9766f496c8845b45393
knaub <eknaub404@gmail.com>
2025-04-26 00:02:09 +0200

    feat: init

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+const page404 = fs.readFileSync("404.html", "utf8");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+
+    var filename = "." + q.pathname;
+    if (filename == "./") {
+      filename = "./index.html";
+    }
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write(page404);
+        return res.end();
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
