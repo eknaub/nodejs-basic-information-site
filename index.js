c14d6a5933cbbed10b3db0cbaea976a6a5370964
knaub <eknaub404@gmail.com>
2025-04-26 03:29:34 +0200

    feat: use express

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-var http = require("http");
-var url = require("url");
-var fs = require("fs");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
 
-const page404 = fs.readFileSync("404.html", "utf8");
+app.get("/", (req, res) => readFile("./index.html", res));
+app.get("/about", (req, res) => readFile("./about.html", res));
+app.get("/contact-me", (req, res) => readFile("./contact-me.html", res));
+app.use((req, res) => readFile("./404.html", res));
 
-http
-  .createServer(function (req, res) {
-    var q = url.parse(req.url, true);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
 
-    var filename = "." + q.pathname;
-    if (!filename.endsWith(".html") && filename !== "./") {
-      filename += ".html";
+const readFile = (filePath, res) => {
+  const absolutePath = path.resolve(filePath);
+
+  fs.readFile(absolutePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(404).send("404 - Page not found");
+      return;
     }
-
-    if (filename === "./") {
-      filename = "./index.html";
-    }
-
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.write(page404);
-        return res.end();
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+    res.status(200).send(data);
+  });
+};
